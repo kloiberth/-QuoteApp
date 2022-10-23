bc9c4e1d593ac2171b953c5652ba7747fb9316ac
Kloiberth Garcia <kloiberthgarcia0902@gmail.com>
2022-10-22 22:50:42 -0500

    button hover

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,7 +3,11 @@ import React from 'react';
 const Button = ({colors, index, change}) => {
     return (
         <div className='button'>
-            <button onClick={change}><i className="fa-solid fa-circle-chevron-right" style={{color: colors[index]}}></i></button>
+            <button onClick={change}>
+                
+                <i className="fa-solid fa-circle-chevron-right" style={{color: colors[index]}}>
+                </i>
+            </button>
         </div>
     );
 };
